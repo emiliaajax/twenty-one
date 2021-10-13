d4e7463bf9b72b438ca23f950ef8e156deaba60a
Emilia Hansson <eh222yn@student.lnu.se>
2021-10-14 00:58:33 +0200

    Create DrawPile module and DiscardPile module

---
+++ src/DiscardPile.js
@@ -0,0 +1,6 @@
+/**
+ * Module for type DiscardPile.
+ *
+ * @author Emilia Hansson <eh222yn@student.lnu.se>
+ * @version 1.1.0
+ */
---
+++ src/DrawPile.js
@@ -0,0 +1,6 @@
+/**
+ * Module for type DrawPile.
+ *
+ * @author Emilia Hansson <eh222yn@student.lnu.se>
+ * @version 1.1.0
+ */
