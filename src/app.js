05d3903b587949dcc23ef2392b763919648557be
Emilia Hansson <eh222yn@student.lnu.se>
2021-10-17 13:43:47 +0200

    Fix error in if statement when adding default players to 3

--- src/app.js
+++ src/app.js
@@ -11,12 +11,13 @@ import { start } from './game.js'
 import process from 'process'
 
 try {
-  const numberOfPlayers = Number(process.argv.pop())
-  if ((numberOfPlayers > 7 && numberOfPlayers < 20) || numberOfPlayers > 50 || Number.isNaN(numberOfPlayers)) {
+  const howManyPlayers = Array.from(process.argv)
+  let numberOfPlayers = Number(howManyPlayers.pop())
+  if ((numberOfPlayers > 7 && numberOfPlayers < 20) || numberOfPlayers > 50 || (Number.isNaN(numberOfPlayers) && process.argv.length > 2)) {
     process.exitCode = 26
     throw new Error('Invalid number of players')
   }
-  start(numberOfPlayers)
+  start(numberOfPlayers = 3)
 } catch (e) {
   console.error(e.message)
   if (!(e instanceof Error)) {
