5215cd58613aec81681d8a87e86bfa04091ad027
Emilia Hansson <eh222yn@student.lnu.se>
2021-10-28 14:16:31 +0200

    Make a Game class instead of exporting a start function and break down loop into private methods

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@
  * @version 1.1.0
  */
 
-import { start } from './game.js'
+import { Game } from './Game.js'
 import { PlayerError } from './PlayerError.js'
 import { DrawPileError } from './DrawPileError.js'
 import process from 'process'
@@ -20,7 +20,8 @@ try {
   } else if (Number.isNaN(numberOfPlayers)) {
     numberOfPlayers = 3
   }
-  start(numberOfPlayers)
+  const game = new Game(numberOfPlayers)
+  game.start()
 } catch (e) {
   console.error(e.message)
   process.exitCode = 1
--- src/game.js
+++ src/game.js
@@ -16,97 +16,126 @@ import { DiscardPile } from './DiscardPile.js'
  *
  * @param {number} numberOfPlayers The number of players.
  */
-export function start (numberOfPlayers) {
-  // Create 52 playing card and...
-  DrawPile.pile = Deck.create()
-  // ... shuffle them.
-  Deck.shuffle(DrawPile.pile)
-
-  // Create dealer and an array to contain all players
-  const players = []
-  const dealer = new Dealer()
-
-  // Create an empty discard pile
-  DiscardPile.pile = []
-
-  // Create all the players and give them a card each
-  for (let j = 1; j <= numberOfPlayers; j++) {
-    const player = new Player(j)
-    players.push(player)
-    player.cards.push(player.demandACard())
+export class Game {
+  /**
+   * Creates a Javascript DiscardPile instance representing a discard pile.
+   *
+   * @param {number} numberOfPlayers The number of players.
+   */
+  constructor (numberOfPlayers) {
+    this.numberOfPlayers = numberOfPlayers
   }
 
-  // The game loop
-  for (let i = 0; i < players.length; i++) {
-    let finalResult = ''
-    const player = players[i]
-    const playerResult = player.playerHand()
-    if (immediateWin(playerResult)) {
-      finalResult = `${player.toString()}\nDealer   : -\nPlayer wins!\n`
-    }
-    if (isBusted(playerResult)) {
-      finalResult = `${player.toString()} YOU HUGE!\nDealer   : -\nDealer wins!\n`
-    }
-    if (!immediateWin(playerResult) && !isBusted(playerResult)) {
-      const dealerResult = dealer.dealerHand()
-      if (immediateWin(dealerResult)) {
-        finalResult = `${player.toString()}\n${dealer.toString()}\nDealer wins!\n`
-      }
-      if (isBusted(dealerResult)) {
-        finalResult = `${player.toString()}\n${dealer.toString()} YOU HUGE!\nPlayer wins!\n`
-      }
-      if (!immediateWin(dealerResult) && !isBusted(dealerResult)) {
-        finalResult = letUsCompareHands(player, dealer)
-      }
-    }
-    console.log(finalResult)
+  /**
+   * Starts the game and prints game result in console.
+   *
+   */
+  start () {
+    DrawPile.pile = Deck.create()
+    Deck.shuffle(DrawPile.pile)
+    const dealer = new Dealer()
+    const players = this.#createAllPlayersAndHandThemACard()
+    this.#playGameAndPrintResult(players, dealer)
+  }
 
-    // Throw dealer and player hand of cards to discard pile
-    let allCards = player.cards.concat(dealer.cards)
+  /**
+   * Creates an array with all the players and gives them one card each.
+   *
+   * @returns {object[]} Returns an array with all players.
+   */
+  #createAllPlayersAndHandThemACard () {
+    const playerArray = []
+    for (let j = 1; j <= this.numberOfPlayers; j++) {
+      const player = new Player(j)
+      playerArray.push(player)
+      player.cards.push(player.demandACard())
+    }
+    return playerArray
+  }
+
+  /**
+   * Dealer plays towards all players one at a time, and the result is printed in console.
+   *
+   * @param {object[]} players An array with all player objects.
+   * @param {object[]} dealer A dealer object.
+   */
+  #playGameAndPrintResult (players, dealer) {
+    for (let i = 0; i < players.length; i++) {
+      let finalResult = ''
+      const player = players[i]
+      const playerResult = player.playerHand()
+      if (this.#immediateWin(playerResult)) {
+        finalResult = `${player.toString()}\nDealer   : -\nPlayer wins!\n`
+      }
+      if (this.#isBusted(playerResult)) {
+        finalResult = `${player.toString()} YOU HUGE!\nDealer   : -\nDealer wins!\n`
+      }
+      if (!this.#immediateWin(playerResult) && !this.#isBusted(playerResult)) {
+        const dealerResult = dealer.dealerHand()
+        if (this.#immediateWin(dealerResult)) {
+          finalResult = `${player.toString()}\n${dealer.toString()}\nDealer wins!\n`
+        }
+        if (this.#isBusted(dealerResult)) {
+          finalResult = `${player.toString()}\n${dealer.toString()} YOU HUGE!\nPlayer wins!\n`
+        }
+        if (!this.#immediateWin(dealerResult) && !this.#isBusted(dealerResult)) {
+          finalResult = this.#compareHands(player, dealer)
+        }
+      }
+      console.log(finalResult)
+      this.#throwCardsOnDiscardPile(player, dealer)
+    }
+  }
+
+  /**
+   * Throws player and dealer cards on discard pile after a round.
+   *
+   * @param {object} player The player object.
+   * @param {object} dealer The dealer object.
+   */
+  #throwCardsOnDiscardPile (player, dealer) {
+    const allCards = player.cards.concat(dealer.cards)
     player.cards = []
     dealer.cards = []
     for (let k = 0; k < allCards.length; k++) {
       DiscardPile.pile.push(allCards[k])
     }
-    allCards = []
   }
-}
 
-// //HELP FUNCTIONS// //
-
-/**
- * Returns true if the result is a win, false otherwise.
- *
- * @param {string} result The result of the hand.
- * @returns {boolean} True if the result is a win, false otherwise.
- */
-function immediateWin (result) {
-  return result === 'WIN'
-}
-
-/**
- * Returns true if the result is lose, false otherwise.
- *
- * @param {string} result The result of the hand.
- * @returns {boolean} True if the result is lose, false otherwise.
- */
-function isBusted (result) {
-  return result === 'LOSE'
-}
-
-/**
- * Returns the final result after comparing player and dealer hands.
- *
- * @param {object} player The current player object.
- * @param {object} dealer The dealer object.
- * @returns {string} A string with the final result.
- */
-function letUsCompareHands (player, dealer) {
-  let finalResult = ''
-  if (player.sum > dealer.sum) {
-    finalResult = `${player.toString()}\n${dealer.toString()}\nPlayer wins!\n`
-  } else {
-    finalResult = `${player.toString()}\n${dealer.toString()}\nDealer wins!\n`
+  /**
+   * Returns true if the result is a win, false otherwise.
+   *
+   * @param {string} result The result of the hand.
+   * @returns {boolean} True if the result is a win, false otherwise.
+   */
+  #immediateWin (result) {
+    return result === 'WIN'
   }
-  return finalResult
+
+  /**
+   * Returns true if the result is lose, false otherwise.
+   *
+   * @param {string} result The result of the hand.
+   * @returns {boolean} True if the result is lose, false otherwise.
+   */
+  #isBusted (result) {
+    return result === 'LOSE'
+  }
+
+  /**
+   * Returns the final result after comparing player and dealer hands.
+   *
+   * @param {object} player The current player object.
+   * @param {object} dealer The dealer object.
+   * @returns {string} A string with the final result.
+   */
+  #compareHands (player, dealer) {
+    let finalResult = ''
+    if (player.sum > dealer.sum) {
+      finalResult = `${player.toString()}\n${dealer.toString()}\nPlayer wins!\n`
+    } else {
+      finalResult = `${player.toString()}\n${dealer.toString()}\nDealer wins!\n`
+    }
+    return finalResult
+  }
 }
